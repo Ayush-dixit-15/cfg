c469162154fad36bbc4b1658fc2c60ade248dbfd
Manish-XD <manish.pgl41@gmail.com>
2022-08-24 17:42:09 +0530

    removed 7-8 bugs for deploying

--- pages/Tnc.js
+++ pages/Tnc.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from "../components/Navbar";
 import styles from '../styles/Tnc.module.css';
-import Footer from '../components/Footer';
 
 
 const Tnc = ({ subTotal, product }) => {
