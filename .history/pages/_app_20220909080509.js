ad1fa4155c59c196016635e456b4aa9855198ea3
unknown <ayushdixit632@gmail.com>
2022-09-09 09:50:42 +0530

    whatsapp

---
+++ .history/pages/_app_20220909080509.js
@@ -0,0 +1,244 @@
+import "../styles/globals.css";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import Script from "next/script";
+import LoadingBar from 'react-top-loading-bar';
+import WhatsApp from "@mui/icons-material/WhatsApp";
+import Whatsapp from "../public/2.png"
+
+function MyApp({ Component, pageProps }) {
+  const [product, setProduct] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+  const [gameCart, setgameCart] = useState({});
+  const [gameTotal, setgameTotal] = useState(0);
+  const [comboCart, setcomboCart] = useState({});
+  const [comboTotal, setcomboTotal] = useState(0);
+  const [optCart, setoptCart] = useState({});
+  const [optTotal, setoptTotal] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+  useEffect(() => {router.events.on('routeChangeStart', ()=>{
+    setProgress(40)
+  })
+  router.events.on('routeChangeComplete', ()=>{
+    setProgress(100)
+  })
+    try {
+      if (localStorage.getItem("product")) {
+        setProduct(JSON.parse(localStorage.getItem("product")));
+        saveProduct(JSON.parse(localStorage.getItem("product")));
+      }
+      if (localStorage.getItem("gameCart")) {
+        setgameCart(JSON.parse(localStorage.getItem("gameCart")));
+        saveGameCart(JSON.parse(localStorage.getItem("gameCart")));
+      }
+      if (localStorage.getItem("comboCart")) {
+        setcomboCart(JSON.parse(localStorage.getItem("comboCart")));
+        saveComboCart(JSON.parse(localStorage.getItem("comboCart")));
+      }
+      if (localStorage.getItem("optCart")) {
+        setoptCart(JSON.parse(localStorage.getItem("optCart")));
+        saveOptCart(JSON.parse(localStorage.getItem("optCart")));
+      }
+    } catch (error) {
+      console.error(error)
+      localStorage.clear()
+    }
+  }, [])
+
+  const saveProduct = (myProduct) => {
+    localStorage.setItem("product", JSON.stringify(myProduct))
+    let subt = 0;
+    let keys = Object.keys(myProduct);
+    for (let i = 0; i < keys.length; i++) {
+      subt += myProduct[keys[i]]["price"] * myProduct[keys[i]].qty;
+    }
+    setSubTotal(subt);
+  }
+  const saveGameCart = (myGameCart) => {
+    localStorage.setItem("gameCart", JSON.stringify(myGameCart))
+    let subt = 0;
+    let keys = Object.keys(myGameCart);
+    for (let i = 0; i < keys.length; i++) {
+      subt += myGameCart[keys[i]]["price"] * myGameCart[keys[i]].qty;
+    }
+    setgameTotal(subt);
+  }
+  const saveComboCart = (myComboCart) => {
+    localStorage.setItem("comboCart", JSON.stringify(myComboCart))
+    let subt = 0;
+    let keys = Object.keys(myComboCart);
+    for (let i = 0; i < keys.length; i++) {
+      subt += myComboCart[keys[i]]["price"] * myComboCart[keys[i]].qty;
+    }
+    setcomboTotal(subt);
+  }
+  const saveOptCart = (myOptCart) => {
+    localStorage.setItem("optCart", JSON.stringify(myOptCart))
+    let subt = 0;
+    let keys = Object.keys(myOptCart);
+    for (let i = 0; i < keys.length; i++) {
+      subt += myOptCart[keys[i]]["price"] * myOptCart[keys[i]].qty;
+    }
+    setoptTotal(subt);
+  }
+
+  
+  const addProductToCart = (productSlug, qty, price, name, img) => {
+    let newProduct = product;
+    if (productSlug in product) {
+      newProduct[productSlug].qty = product[productSlug].qty + qty;
+    }
+    else {
+      newProduct[productSlug] = { qty: 1, price, name, img }
+    }
+    router.push(`/Games`);
+    setProduct(newProduct);
+    saveProduct(newProduct);
+  }
+  const addGameCartToCart = (productSlug, qty, price, name, img) => {
+    let newProduct = gameCart;
+    if (productSlug in gameCart) {
+      newProduct[productSlug].qty = gameCart[productSlug].qty + qty;
+    }
+    else {
+      newProduct[productSlug] = { qty: 1, price, name, img }
+    }
+    setgameCart(newProduct);
+    saveGameCart(newProduct);
+  }
+  const addComboCartToCart = (productSlug, qty, price, name, img) => {
+    let newProduct = comboCart;
+    if (productSlug in comboCart) {
+      newProduct[productSlug].qty = comboCart[productSlug].qty + qty;
+    }
+    else {
+      newProduct[productSlug] = { qty: 1, price, name, img }
+    }
+    router.push(`/KYC`);
+    setcomboCart(newProduct);
+    saveComboCart(newProduct);
+  }
+  const addOptCartToCart = (productSlug, qty, price, name, img) => {
+    let newProduct = optCart;
+    if (productSlug in optCart) {
+      newProduct[productSlug].qty = optCart[productSlug].qty + qty;
+    }
+    else {
+      newProduct[productSlug] = { qty: 1, price, name, img }
+    }
+    setoptCart(newProduct);
+    saveOptCart(newProduct);
+  }
+
+  const clearProduct = (productSlug) => {
+    delete product[productSlug]; 
+    setProduct(product);
+    saveProduct(product);
+  }
+  const clearGameCart = (productSlug) => {
+    delete gameCart[productSlug]; 
+    setgameCart(gameCart);
+    saveGameCart(gameCart);
+  }
+  const clearComboCart = (productSlug) => {
+    delete comboCart[productSlug];
+    setcomboCart(comboCart);
+    saveComboCart(comboCart);
+  }
+  const clearOptCart = (productSlug) => {
+    delete optCart[productSlug];
+    setoptCart(optCart);
+    saveOptCart(optCart);
+  }
+
+  const removeProductFromCart = (productSlug, qty, price, name, img) => {
+    let newProduct = JSON.parse(JSON.stringify(product));
+    if (productSlug in product) {
+      newProduct[productSlug].qty = product[productSlug].qty - qty;
+    }
+    if (newProduct[productSlug]["qty"] <= 0) {
+      delete newProduct[productSlug]
+    }
+    setProduct(newProduct);
+    saveProduct(newProduct);
+  }
+  const removeGameCartFromCart = (productSlug, qty, price, name, img) => {
+    let newProduct = JSON.parse(JSON.stringify(gameCart));
+    if (productSlug in gameCart) {
+      newProduct[productSlug].qty = gameCart[productSlug].qty - qty;
+    }
+    if (newProduct[productSlug]["qty"] <= 0) {
+      delete newProduct[productSlug]
+    }
+    setgameCart(newProduct);
+    saveGameCart(newProduct);
+  }
+  const removeComboCartFromCart = (productSlug, qty, price, name, img) => {
+    let newProduct = JSON.parse(JSON.stringify(comboCart));
+    if (productSlug in comboCart) {
+      newProduct[productSlug].qty = comboCart[productSlug].qty - qty;
+    }
+    if (newProduct[productSlug]["qty"] <= 0) {
+      delete newProduct[productSlug]
+    }
+    setcomboCart(newProduct);
+    saveComboCart(newProduct);
+  }
+  const removeOptCartFromCart = (productSlug, qty, price, name, img) => {
+    let newProduct = JSON.parse(JSON.stringify(optCart));
+    if (productSlug in optCart) {
+      newProduct[productSlug].qty = optCart[productSlug].qty - qty;
+    }
+    if (newProduct[productSlug]["qty"] <= 0) {
+      delete newProduct[productSlug]
+    }
+    setoptCart(newProduct);
+    saveOptCart(newProduct);
+  }
+//   <script async src="https://www.googletagmanager.com/gtag/js?id=G-K48YYQ1Z1G"></script>
+// <script>
+  // window.dataLayer = window.dataLayer || [];
+  // function gtag(){dataLayer.push(arguments);}
+  // gtag('js', new Date());
+
+  // gtag('config', 'G-K48YYQ1Z1G');
+// </script>
+  return (
+    <>
+      <Head>
+        <script src="https://accounts.google.com/gsi/client" async defer></script>
+      </Head>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-K48YYQ1Z1G"/>
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-K48YYQ1Z1G');
+        `}
+      </Script>
+      <LoadingBar
+      color='#FF0000'
+      waitingTime={400}
+      progress={progress}
+      onLoaderFinished={() => setProgress(0)}
+    />
+      <Component product={product} addProductToCart={addProductToCart} removeProductFromCart={removeProductFromCart} clearProduct={clearProduct} subTotal={subTotal} gameCart={gameCart} gameTotal={gameTotal} comboCart={comboCart} comboTotal={comboTotal} optCart={optCart} optTotal={optTotal} addGameCartToCart={addGameCartToCart} removeGameCartFromCart={removeGameCartFromCart} clearGameCart={clearGameCart} addOptCartToCart={addOptCartToCart} removeOptCartFromCart={removeOptCartFromCart} clearOptCart={clearOptCart} addComboCartToCart={addComboCartToCart} removeComboCartFromCart={removeComboCartFromCart} clearComboCart={clearComboCart} {...pageProps}/>
+      <a href = "https://wa.me/918287702693" target="_blank">
+      <WhatsApp
+               style={{
+                position:"fixed",
+                bottom:"40px",
+                right:"20px"
+            }}
+        className="cursor-pointer" />
+      </a>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ .history/pages/consoles/[slug]_20220829101724.js
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import styles from "../../styles/Console.module.css";
+import SanitizerIcon from '@mui/icons-material/Sanitizer';
+import VerifiedOutlinedIcon from '@mui/icons-material/VerifiedOutlined';
+import Link from "next/link";
+import Carousel from 'react-elastic-carousel';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import controller from '../../public/controller.png';
+import Image from "next/image";
+import Accordion from "../../components/Accordion";
+
+const Console = ({ console, product, subTotal, addProductToCart, removeProductFromCart, clearProduct, gameCart, gameTotal, comboCart, comboTotal, optCart, optTotal, addGameCartToCart, removeGameCartFromCart, clearGameCart, addOptCartToCart, removeOptCartFromCart, clearOptCart, addComboCartToCart, removeComboCartFromCart, clearComboCart }) => {
+    const breakPoint = [
+        { width: 1, itemsToShow: 2 },
+        { width: 800, itemsToShow: 2 },
+        { width: 768, itemsToShow: 4 },
+        { width: 1200, itemsToShow: 4 }
+      ];
+    const router = useRouter();
+    const { slug } = router.query;
+    const [count, setcount] = useState(0);
+    return (
+        <div>
+            <Navbar
+                product={product} addProductToCart={addProductToCart} removeProductFromCart={removeProductFromCart} clearProduct={clearProduct} subTotal={subTotal} gameCart={gameCart} gameTotal={gameTotal} comboCart={comboCart} comboTotal={comboTotal} optCart={optCart} optTotal={optTotal} addGameCartToCart={addGameCartToCart} removeGameCartFromCart={removeGameCartFromCart} clearGameCart={clearGameCart} addOptCartToCart={addOptCartToCart} removeOptCartFromCart={removeOptCartFromCart} clearOptCart={clearOptCart} addComboCartToCart={addComboCartToCart} removeComboCartFromCart={removeComboCartFromCart} clearComboCart={clearComboCart}
+                KYC={"KYC"}
+                About={"About"}
+                Contact={"Contact"}
+                Login={"Login"}
+                Signup={"Signup"}
+                key={product}
+            />
+            <div className={styles.landing}>
+                <div className={styles.left}>
+                    <img src={console.attributes.mainImage.data.attributes.url} alt="" />
+                    <div className={styles.extraImage}>
+                        <img src={console.attributes.extraImage1.data.attributes.url} alt="" />
+                        <img src={console.attributes.extraImage2.data.attributes.url} alt="" />
+                        <img src={console.attributes.extraImage3.data.attributes.url} alt="" />
+                    </div>
+                    <div className={styles.qualities}>
+                        <div className={styles.quality}>
+                            <SanitizerIcon />
+                            <span>Well Sanitized</span>
+                        </div>
+                        <div className={styles.quality}>
+                            <VerifiedOutlinedIcon />
+                            <span>Quality Assured</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <h2>{console.attributes.title}</h2>
+                    <h4>Details</h4>
+                    <p>{console.attributes.description}</p>
+                    <div className={styles.plan}>
+                        <h2>Choose your plan</h2>
+                        <form action="">
+                            <div className={styles.plan_item}>
+                                <input type="radio" name="plan" id="plan" onClick={() => { setcount(1) }} />
+                                <h3>₹{console.attributes.plan1}</h3>
+                            </div>
+                            <hr />
+                            <div className={styles.plan_item}>
+                                <input type="radio" name="plan" id="plan" onClick={() => { setcount(2) }} />
+                                <h3>₹{console.attributes.plan2}</h3>
+                            </div>
+                            <hr />
+                            <div className={styles.plan_item}>
+                                <input type="radio" name="plan" id="plan" onClick={() => { setcount(3) }} />
+                                <h3>₹{console.attributes.plan3}</h3>
+                            </div>
+                            <hr />
+                            <div className={styles.plan_item}>
+                                <input type="radio" name="plan" id="plan" onClick={() => { setcount(4) }} />
+                                <h3>₹{console.attributes.plan4}</h3>
+                            </div>
+                            <hr />
+                            {count != 0 &&<div className={styles.gamesBtn}>
+                                 <p onClick={() => { addProductToCart(slug, 1, eval(`console.attributes.plan${count}`), console.attributes.title, console.attributes.mainImage.data.attributes.url) }}>Proceed to buy games</p>
+                            </div>}
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.margining}>
+            <div className={styles.topic}>
+                    <h1>What&apos;s in the Box?</h1>
+                    <hr />
+                </div>
+            <Carousel breakPoints={breakPoint}>
+                <div className={styles.what}>attribute
+                    <img src={console.attributes.whatsInTheBoxImage1.data.attributes.url}/>
+                    <span>{console.attributes.whatsInTheBoxText1}</span>
+                </div>
+                <div className={styles.what}>
+                    <img src={console.attributes.whatsInTheBoxImage2.data.attributes.url}/>
+                    <span>{console.attributes.whatsInTheBoxText2}</span>
+                </div>
+                <div className={styles.what}>
+                    <img src={console.attributes.whatsInTheBoxImage3.data.attributes.url}/>
+                    <span>{console.attributes.whatsInTheBoxText3}</span>
+                </div>
+                <div className={styles.what}>
+                    <img src={console.attributes.whatsInTheBoxImage4.data.attributes.url}/>
+                    <span>{console.attributes.whatsInTheBoxText4}</span>
+                </div>
+                <div className={styles.what}>
+                    <img src={console.attributes.whatsInTheBoxImage5.data.attributes.url}/>
+                    <span>{console.attributes.whatsInTheBoxText5}</span>
+                </div>
+            </Carousel>
+            <div className={styles.topic}>
+                    <h1>Add these: (Optional)</h1>
+                    <hr />
+                </div>
+            <div className={styles.optional}>
+                <div className={styles.left}>
+                    <Image src={controller}></Image>
+                </div>
+                <div className={styles.right}>
+                    <h2>Extra PS4 Controller</h2>
+                    <p style={{color: "var(--red)"}}>₹ 100</p>
+                    <div className={styles.add_sub}>
+                        {(optCart.controller!= undefined || optCart.controller!=null) && <button style={{color: "var(--red)"}}><RemoveIcon onClick={()=>{
+                            removeOptCartFromCart("controller",1,100,"PS4 controller", "https://mutterfly.imgix.net/flamingo/addons/Extra%20PS4%20Controller.jpg?w=338.8235294117647&auto=format,compress&q=60")
+                        }}/></button>}
+                        {(optCart.controller=== undefined || optCart.controller===null) && <button style={{color: "var(--red)"}}><RemoveIcon/></button>}
+                        {(optCart.controller!= undefined || optCart.controller!=null) && <span>{`${optCart.controller.qty}`}</span>}
+                        {(optCart.controller=== undefined || optCart.controller===null) && <span>0</span>}
+                        <button style={{color: "var(--red)"}} onClick={()=>{
+                            addOptCartToCart("controller",1,100,"PS4 controller", "https://mutterfly.imgix.net/flamingo/addons/Extra%20PS4%20Controller.jpg?w=338.8235294117647&auto=format,compress&q=60")
+                        }}><AddIcon/></button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.topic}>
+                    <h1>Frequently Asked Questions</h1>
+                    <hr />
+                </div>
+            <Accordion/>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export async function getServerSideProps(context) {
+    let headers = {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_READ}`,
+    };
+    let url =
+        process.env.NEXT_PUBLIC_STRAPI_HOST + "/api/consoles?filters[slug]=" +
+        context.query.slug +
+        "&populate=*";
+    let data = await fetch(url, { headers: headers });
+    let console = await data.json();
+    return {
+        props: { console: console.data[0] }, // will be passed to the page component as props
+    };
+}
+
+export default Console;
